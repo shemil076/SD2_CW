6e60788d5dfb413230ec7667440791b269555b32
Pramuditha Karunarathna <shemil076@gmail.com>
2021-07-09 20:13:29 +0530

    started class version

---
+++ ClassesVersion/src/Booth.java
@@ -0,0 +1,13 @@
+public class Booth {
+    private int boothNumber;
+    private String patientName;
+
+
+    public Booth(int boothNumber, String patientName)
+    {
+        this.boothNumber = boothNumber;
+        this.patientName = patientName;
+    }
+
+
+}
---
+++ ClassesVersion/src/VaccinationCenter.java
@@ -0,0 +1,6 @@
+public class VaccinationCenter {
+
+    public static void main(String[] args) {
+        Booth[] booths = new Booth[6];
+    }
+}
