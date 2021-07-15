d66de68e475d9e21797b971ce42109695ac9049c
Pramuditha Karunarathna <shemil076@gmail.com>
2021-07-16 04:28:09 +0530

    javafx done

--- JavaFX/src/sample/formPageController.java
+++ JavaFX/src/sample/formPageController.java
@@ -1,4 +1,214 @@
 package sample;
 
-public class formPageController {
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class formPageController   {
+    ObservableList list = FXCollections.observableArrayList();
+    public javafx.scene.control.Button generateReceipt;
+
+
+    @FXML
+    private ImageView body;
+
+    @FXML
+    private ToggleGroup gender;
+
+
+    @FXML
+    private TextField city;
+
+    @FXML
+    private TextField contactNumber;
+
+    @FXML
+    private TextField idNumber;
+
+    @FXML
+    private TextField mail;
+
+    @FXML
+    private TextField age;
+
+
+    @FXML
+    public TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private RadioButton radioMale;
+
+
+    @FXML
+    private RadioButton radioFemale;
+
+
+    @FXML
+    private RadioButton astrazeneca;
+
+    @FXML
+    private ToggleGroup vaccines;
+
+    @FXML
+    private RadioButton sinopharm;
+
+    @FXML
+    private RadioButton pfizer;
+
+    public String getBoothNumber() {
+        String vaccine = loadVaccines();
+
+        int boothNumber = 0;
+        if (vaccine.equalsIgnoreCase("astrazeneca")){
+            boothNumber = (int)Math.random() *1;
+
+        }else if (vaccine.equalsIgnoreCase("sinopharm")){
+            boothNumber =(int) (Math.random() * ((3 - 2) + 1)) +2;
+        }else if(vaccine.equalsIgnoreCase("pfizer")){
+            boothNumber =(int) (Math.random() * ((5 - 4) + 1)) +4;
+        }
+       String boothNo= String.valueOf(boothNumber);
+        return boothNo;
+    }
+
+
+
+    public String loadVaccines(){
+        String vaccineType ="";
+        if (astrazeneca.isSelected()){
+            vaccineType += astrazeneca.getText();
+        }else if(sinopharm.isSelected()){
+            vaccineType += sinopharm.getText();
+        }else if(pfizer.isSelected()){
+            vaccineType += pfizer.getText();
+        }
+        return  vaccineType;
+    }
+
+    public String loadRadio(){
+        String patientsGender ="";
+        if (radioMale.isSelected()){
+            patientsGender += radioMale.getText();
+        }else if(radioFemale.isSelected()){
+            patientsGender += radioFemale.getText();
+        }
+        return  patientsGender;
+    }
+
+
+
+    @FXML
+    public void sendDetails(ActionEvent actionEvent) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("receipt.fxml"));
+        Parent root = loader.load();
+
+        receiptController getReceipt = loader.getController();
+        String first_name = firstName.getText();
+        String last_name = lastName.getText();
+        String e_Mail = mail.getText();
+        String id_number = idNumber.getText();
+        String patient_age =  age.getText();
+        String current_city =  city.getText();
+        String contact_number = contactNumber.getText();
+        String patientsGender = loadRadio() ;
+        String vaccine = loadVaccines();
+
+
+
+
+
+        if (first_name.equals("")){
+            Alert newAlert = new Alert(Alert.AlertType.INFORMATION);
+            newAlert.setContentText("First name is missing, Enter the first name");
+            newAlert.show();
+        }else{
+            if (last_name.equals("")){
+                Alert newAlert = new Alert(Alert.AlertType.INFORMATION);
+                newAlert.setContentText("Last name is missing, Enter the last name");
+                newAlert.show();
+            }else{
+
+                if(current_city.equals("")){
+                    Alert newAlert = new Alert(Alert.AlertType.INFORMATION);
+                    newAlert.setContentText("Current living city is missing, Enter the current city address");
+                    newAlert.show();
+                }else{
+                    if (id_number.equals("")){
+                        Alert newAlert = new Alert(Alert.AlertType.INFORMATION);
+                        newAlert.setContentText("your NIC/PASSPORT number is missing, Enter your NIC/PASSPORT number address");
+                        newAlert.show();
+                    }else{
+                        if (patient_age.equals("")){
+                            Alert newAlert = new Alert(Alert.AlertType.INFORMATION);
+                            newAlert.setContentText("Your age is missing, Enter the age address");
+                            newAlert.show();
+                        }else{
+                            if (contact_number.equals("") ){
+                                Alert newAlert = new Alert(Alert.AlertType.INFORMATION);
+                                newAlert.setContentText("Your contact number is missing, Enter the contact number address");
+                                newAlert.show();
+                            }else{
+                                if(e_Mail.equals("")){
+                                    Alert newAlert = new Alert(Alert.AlertType.INFORMATION);
+                                    newAlert.setContentText("Your e-mail address  is missing, Enter the E-mail address");
+                                    newAlert.show();
+                                }else{
+                                    if(patientsGender.equals("")){
+                                        Alert newAlert = new Alert(Alert.AlertType.INFORMATION);
+                                        newAlert.setContentText("Your gender is missing, Enter the the gender address");
+                                        newAlert.show();
+
+                                    } else{
+                                        if(vaccine.equals("")){
+                                            Alert newAlert = new Alert(Alert.AlertType.INFORMATION);
+                                            newAlert.setContentText("Please select a vaccine type");
+                                            newAlert.show();
+
+                                        } else{
+
+                                                String booths =getBoothNumber();
+                                            System.out.println(booths);
+
+
+                                            getReceipt.showReceipt(first_name,last_name,e_Mail,id_number,patient_age,current_city,contact_number,patientsGender, vaccine, booths );
+
+                                            Stage newStage = new Stage();
+                                            newStage.setScene(new Scene(root,878,496));
+                                            newStage.setTitle("Vaccination Receipt");
+                                            newStage.show();
+                                            Stage previousStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                                            previousStage.close();
+                                        }
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
+
--- JavaFX/src/sample/receiptController.java
+++ JavaFX/src/sample/receiptController.java
@@ -1,4 +1,98 @@
 package sample;
 
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class receiptController {
+    @FXML
+    private AnchorPane body;
+
+    @FXML
+    public Label lblFirstName;
+
+    @FXML
+    private Label lblGender;
+
+    @FXML
+    private Label lblAge;
+
+    @FXML
+    private Label lblLastName;
+
+    @FXML
+    private Label lblVaccine;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    private Label lblContactNumber;
+
+    @FXML
+    private Label lblEmail;
+
+    @FXML
+    private Label lblId;
+
+
+    @FXML
+    private Label lblbooth;
+
+
+    @FXML
+    private Label lblCity;
+
+    @FXML
+    private Button btnClose;
+
+
+
+
+    public void close(javafx.event.ActionEvent actionEvent) throws IOException {
+
+        Stage previousStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        previousStage.close();
+    }
+
+    public void showReceipt(String firstName, String lastName, String mail, String idNumber, String age , String city, String contactNumber, String gender, String vaccine, String booth){
+        lblFirstName.setText("  "+ firstName);
+        lblLastName.setText("  "+ lastName);
+        lblEmail.setText("  "+ mail);
+        lblId.setText("  "+ idNumber);
+        lblAge.setText("  "+ age);
+        lblCity.setText("  "+ city);
+        lblContactNumber.setText("  "+ contactNumber);
+        lblGender.setText("  "+gender);
+        lblVaccine.setText("  "+vaccine);
+        lblbooth.setText("  "+booth);
+
+
+
+
+        Date currentDate = new Date();
+        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss:a");
+        lblTime.setText(timeFormat.format(currentDate));
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        lblDate.setText(dateFormat.format(currentDate));
+
+
+    }
 }
